599e87c9bb802905f967ddbe701c667b6062b582
chidambaram92 <chidambaram1508@gmail.com>
2022-09-11 22:51:59 +0530

    Added few more programs for Strings

---
+++ src/com/src/testarray/StringManipulation.java
@@ -0,0 +1,33 @@
+package com.src.testarray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringManipulation {
+    private String actualValue="chromeData1234567";
+    private String numberAlone=null;
+    private String charAlone=null;
+  public static void main(String[] args) {
+      StringManipulation objOne= new StringManipulation();
+      objOne.verifySplitData();
+  }
+ public void verifySplitData(){
+     numberAlone=actualValue.replaceAll("\\D","");
+     System.out.println("Number Only: "+numberAlone);
+     charAlone=actualValue.replaceAll("\\d","");
+     System.out.println("Character Only: "+charAlone);
+     String splitValue=charAlone.substring(0,6);
+     System.out.println("String First: "+splitValue);
+     String splitValueTwo=charAlone.substring(6,10);
+     System.out.println("String Second: "+splitValueTwo);
+     List<String> arrList= new ArrayList<>();
+     arrList.add(splitValue);
+     arrList.add(splitValueTwo);
+     arrList.add(numberAlone);
+     for(String sVal:arrList){
+         System.out.println(sVal);
+     }
+ }
+
+
+}
